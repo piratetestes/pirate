55fc64f82d438d5062d60fac6f7dcd1d622bbfd9
Twilight <ihatespam@gmail.com>
2023-12-04 01:54:51 -0600

    Update update-theme.sh

--- update-theme.sh
+++ update-theme.sh
@@ -12,19 +12,16 @@ mv src user_src_backup
 # Clone the central repository
 git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
 
-# Replace the src folder
-rm -rf src
+# Replace the src and public folders
+rm -rf src public
 mv tmp_theme/src .
+mv tmp_theme/public .
 
 # Copy the package.json file
 cp tmp_theme/package.json .
 
+echo "Theme updated successfully!"
+
 # Clean up
 rm -rf tmp_theme
 rm -rf user_src_backup
-
-echo "Theme updated successfully!"
-
-# Replace the src folder
-rm -rf public
-mv tmp_theme/public .
