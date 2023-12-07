a000353db5c3958dff00de35436dfb9a49773e32
Twilight <ihatespam@gmail.com>
2023-12-07 03:18:48 -0600

    Update update-theme.sh

--- update-theme.sh
+++ update-theme.sh
@@ -1,28 +1,34 @@
 #!/bin/bash
 
-# # Set the URL of your central repository
-# THEME_REPO_URL="https://github.com/piratesocial/pirateplus"
+# Set the URL of your central repository
+THEME_REPO_URL="https://github.com/piratesocial/pirateplus"
 
-# # Set the branch or tag you want to pull updates from
-# BRANCH_OR_TAG="main"
+# Set the branch or tag you want to pull updates from
+BRANCH_OR_TAG="main"
 
-# # Backup user changes
-# mv src user_src_backup
+# Backup user changes
+mv src user_src_backup
 
-# # Clone the central repository
-# git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
+# Clone the central repository
+git clone --branch $BRANCH_OR_TAG --depth 1 $THEME_REPO_URL tmp_theme
 
-# # Replace the src folder
-# rm -rf src
-# mv tmp_theme/src .
+# Replace the src folder
+rm -rf src
+mv tmp_theme/src .
 
-# # Update the admin/config.yml file
-# cp tmp_theme/static/admin/config.yml static/admin/
+# Replace the gatsby-config.js file
+cp tmp_theme/gatsby-config.js .
 
-# # Copy the package.json file
-# cp tmp_theme/package.json .
+# Replace the gatsby-node.js file
+cp tmp_theme/gatsby-node.js .
 
-# # Clean up
-# rm -rf tmp_theme
+# Update the admin/config.yml file
+cp tmp_theme/static/admin/config.yml static/admin/
+
+# Copy the package.json file
+cp tmp_theme/package.json .
+
+# Clean up
+rm -rf tmp_theme
 
 echo "Theme updated successfully!"
